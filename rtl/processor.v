e00a9648bcf52cb7b40a40183e9a8f09d8878cf3
belang <feiying3521@163.com>
2017-05-18 21:48:19 +0800

    build the top module version one.

--- rtl/processor.v
+++ rtl/processor.v
@@ -14,30 +14,69 @@ input clk, rst_n;
 ir_regifle ir_regifle_0(
     .clk (clk),
     .rst_n (rst_n),
-    regfile_en,
-    .regfile_selection (ir_regfile_selection),
-    read_or_write_en,
-    .address (ir_pointer),,
-    .i_data (cached_data),
-    .o_data (ir),
+    .i_regfile_en (ir_regfile_en),
+    .i_regfile_selection (ir_regfile_selection),
+    .i_read_or_write_en (ir_read_or_write_en),
+    .i_address (ir_pointer),
+    //.i_data (cached_data),
+    .o_data (ir)
 );
 
 fetch fetch_0(
     .clk (clk),
     .rst_n (rst_n),
     //fetch_en,
-    target_device_flag,
-    device,
-    address,
+    .o_target_device_flag (target_device_flag),
+    .o_device (device),
+    .o_address (address)
 );
 
 control control_0(
-    device,
-    address,
-    ir_regfile_en,
-    ir_regfile_selection,
-    ir_read_or_write_en,
-    ir_pointer,
+    .clk (clk),
+    .rst_n (rst_n),
+    .i_device (device),
+    .i_address (address),
+    .o_ir_regfile_en (ir_regfile_en),
+    .o_ir_regfile_selection (ir_regfile_selection),
+    .o_ir_read_or_write_en (ir_read_or_write_en),
+    .o_ir_pointer (ir_pointer)
     //fetch_en,
 );
+
+data_regifle data_regifle_0(
+    .clk (clk),
+    .rst_n (rst_n),
+    .i_device (device),
+    .i_address (address),
+    .i_data (cached_data),
+    .o_data (data_to_cache)
+);
+
+data_cache data_cache_0(
+    .clk (clk),
+    .rst_n (rst_n),
+    .i_device (device),
+    .i_address (address),
+    .i_data (data_to_cache),
+    .o_data (cached_data)
+);
+
+compute compute_0(
+    .clk (clk),
+    .rst_n (rst_n),
+    .i_device (device),
+    .i_address (address),
+    .i_data (cached_data),
+    .o_data (data_to_cache)
+);
+
+interface interface_0(
+    .clk (clk),
+    .rst_n (rst_n),
+    .i_device (device),
+    .i_address (address),
+    .i_data (cached_data),
+    .o_data (data_to_cache)
+);
+
 endmodule
